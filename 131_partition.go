77541e8e040dfb09ba522e7eb9a3a695509f950c
hoswey <hongshuwei@gmail.com>
2020-11-20 09:27:24 +0800

    使用动态规划预计算回文串

--- 131_partition.go
+++ 131_partition.go
@@ -7,42 +7,57 @@
 // @lc code=start
 func partition(s string) [][]string {
 
+	if len(s) == 0 {
+		return nil
+	}
+
+	dp := calParlindrome(s)
 	var ans [][]string
-	backtracking(s, nil, &ans)
+	backtracking(s, nil, &ans, dp, 0)
 	return ans
 }
 
-func backtracking(s string, path []string, ans *[][]string) {
+func backtracking(s string, path []string, ans *[][]string, dp [][]bool, from int) {
 
 	if len(s) == 0 {
 		*ans = append(*ans, path)
 		return
 	}
 	for i := 1; i <= len(s); i++ {
-		if isParlindrome(s[0:i]) {
+		if dp[from][from+i-1] {
 			newPath := make([]string, len(path))
 			copy(newPath, path)
 			newPath = append(newPath, s[0:i])
-			backtracking(s[i:], newPath, ans)
+			backtracking(s[i:], newPath, ans, dp,from+i)
 		}
 	}
 }
 
-func isParlindrome(s string) bool {
+func calParlindrome(s string) [][]bool {
 
-	if len(s) <= 1 {
-		return true
+	dp := make([][]bool, len(s))
+	for i := range dp {
+		dp[i] = make([]bool, len(s))
 	}
-	l := 0
-	r := len(s) - 1
-	for l < r {
-		if s[l] != s[r] {
-			return false
+
+	for i := 0; i < len(s); i++{
+		dp[i][i] = true
+	}
+
+	for i := 0; i < len(s) - 1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
 		}
-		l++
-		r--
 	}
-	return true
+
+	for l := 3; l <= len(s); l++ {
+		for i := 0; i <= len(s) - l; i++ {
+			if s[i] == s[i+l-1] && dp[i+1][i+l-2] {
+				dp[i][i+l-1] = true
+			}
+		}
+	}
+	return dp
 }
 // @lc code=end
 
