d64407b5b24a58e77c24746aae19c809f906d7b4
hongshuwei1 <hongshuwei1@joyy.sg>
2021-10-02 10:04:29 +0800

    misc

--- 148.sort-list.go
+++ 148.sort-list.go
@@ -13,66 +13,66 @@
  * }
  */
 func sortList(head *ListNode) *ListNode {
+
 	return mergeSort(head)
 }
 
 func mergeSort(head *ListNode) *ListNode {
 
-	if head == nil || head.Next == nil {
+	if head == nil || head.Next == nil{
 		return head
 	}
 
-	l, r := partition(head)
-	l = mergeSort(l)
-	r = mergeSort(r)
+	d := new(ListNode)
+	d.Next = head
 
-	return merge(l, r)
-}
-
-func partition(head *ListNode) (*ListNode, *ListNode) {
-
-	dummy := new(ListNode)
-	dummy.Next = head
-
-	slow, fast := dummy, dummy
-	for fast != nil && fast.Next != nil {
+	fast, slow := d, d
+	for fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next
-		if fast != nil {
+		if fast.Next != nil {
 			fast = fast.Next
 		}
 	}
 
-	next := slow.Next
+	l1 := d.Next
+	l2 := slow.Next
 	slow.Next = nil
-	return dummy.Next, next
+
+	l1 = mergeSort(l1)
+	l2 = mergeSort(l2)
+
+	return merge(l1, l2)
 }
 
-func merge(l1 *ListNode, l2 *ListNode) *ListNode {
-
-	dummy := new(ListNode)
-	cur := dummy
-
+func merge(l1, l2 *ListNode) *ListNode {
+	
+	d := new(ListNode)
+	cur := d
 	for l1 != nil && l2 != nil {
+		
 		if l1.Val < l2.Val {
 			cur.Next = l1
+			cur = cur.Next
+
 			l1 = l1.Next
 		} else {
 			cur.Next = l2
+			cur = cur.Next
+
 			l2 = l2.Next
 		}
-		cur = cur.Next
 	}
 
 	if l1 != nil {
 		cur.Next = l1
-	} 
+	}
 
 	if l2 != nil {
 		cur.Next = l2
 	}
-
-	return dummy.Next
+	
+	return d.Next
 }
 // @lc code=end
 
